b68bfc872d8274fbe410555b71d33dac8a0b1371
Mark Bell <me@markashleybell.com>
2014-01-30 14:23:15 +0000

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, send_from_directory
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+# @app.route('/static.html')
+@app.route('/robots.txt')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
+
+if __name__ == "__main__":
+    app.run()
