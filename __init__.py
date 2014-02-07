3bac25cfb51299eace9cca4b8ac6aa04a51ab44b
Mark Bell <me@markashleybell.com>
2014-02-07 17:32:46 +0000

    Insert code

--- __init__.py
+++ __init__.py
@@ -6,12 +6,15 @@ psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
 from flask import Flask, render_template, request, send_from_directory, redirect, url_for
 app = Flask(__name__)
 
+# Load configuration
 app.config.from_pyfile('config.cfg')
 
+# Normal (non-query) SQL
 list_sql = """
            SELECT * FROM links ORDER BY id LIMIT %s OFFSET %s;
            """
 
+# Tag query SQL
 query_sql = """
             SELECT
                 l1.*
@@ -34,32 +37,44 @@ query_sql = """
 @app.route("/")
 @app.route("/<int:page>")
 def index(page=1):
-    pagesize = 2
+    # Paging variables
+    pagesize = app.config['PAGE_SIZE']
     offset = (page - 1) * pagesize
-    
+    # Optional tag query parameters
     query = request.args.get("q")
     query_terms = []
-
+    # Set up default SQL/parameters
     sql = list_sql
     params = [pagesize, offset]
-
+    # If any tags were passed in as a query
     if query is not None:
         # Try and tidy up the tag query terms a bit
         query_terms = [s.lower().strip() for s in query.split(' ') if s.strip() is not '']
+        # Use the query SQL
         sql = query_sql
         params = [tuple(query_terms), len(query_terms), pagesize, offset]
-    
+    # Get the data
     conn = psycopg2.connect(app.config['CONNECTION_STRING'])
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     cur.execute(sql, params)
     results = cur.fetchall()
     cur.close()
     conn.close()
-
     return render_template('index.html', results=results, query_terms=query_terms)
 
 @app.route("/update-link", methods=['POST'])
 def update_link():
+    link_id = request.form['link_id']
+    title = request.form['title']
+    url = request.form['url']
+    abstract = request.form['abstract']
+    tags = request.form['tags']
+    conn = psycopg2.connect(app.config['CONNECTION_STRING'])
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur.execute("INSERT INTO links (title, url, abstract) VALUES (%s, %s, %s)", [title, url, abstract])
+    conn.commit()
+    cur.close()
+    conn.close()
     return redirect(url_for('index'))
 
 # @app.route('/static.html')
